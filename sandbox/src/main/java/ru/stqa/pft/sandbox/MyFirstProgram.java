392763c48664a9aa969f9646f1fa18f45e1657cf
Chistova Nataliia <chistova.nataliia@yandex.ru>
2017-07-24 18:41:08 +0300

    Добавлены методы Square и Rectangle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,25 @@ public class MyFirstProgram {
     hello("user");
     hello("Nataly");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    double p1 = 5;
+    double q1 = 6;
+    double p2 = 5;
+    double q2 = 6;
+    double dis = Math.sqrt(Math.pow((p1 - q1), 2) + Math.pow((p2 - q2), 2));
+    System.out.println("Расстояние между двумя точками " + "(p1, q1) = " + "(" + p1 + ", " + q1 + ")" + " и " + "(p2, q2) = " + "(" + p2 + ", " + q2 + ")" + " = " + dis);
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double l) {
-    return l * l;
-  }
+ /* public static double distanse(double p1; double p2) {
 
-  public static double area(double a, double b) {
-    return a * b;
-  }
+  }*/
 }
