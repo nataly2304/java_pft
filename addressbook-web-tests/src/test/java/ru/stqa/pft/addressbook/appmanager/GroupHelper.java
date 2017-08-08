fa8a6dba81394cf1a47265c3149f26b8961acdfe
Chistova Nataliia <chistova.nataliia@yandex.ru>
2017-08-08 19:29:16 +0300

    Добавлены классы ApplicationManager, SessionHelper, NavigationHelper

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupHelper {
+  private FirefoxDriver wd;
+
+  public GroupHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  // вернуться на страницу со списком групп
+  public void returnToGroupPage() {
+      wd.findElement(By.linkText("group page")).click();
+  }
+
+  // подтвердить создание группы
+  public void submitGroupCreation() {
+      wd.findElement(By.name("submit")).click();
+  }
+
+  // заполнить форму какими-то данными
+  public void fillGroupForm(GroupData groupData) {
+      wd.findElement(By.name("group_name")).click();
+      wd.findElement(By.name("group_name")).clear();
+      wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+      wd.findElement(By.name("group_header")).click();
+      wd.findElement(By.name("group_header")).clear();
+      wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+      wd.findElement(By.name("group_footer")).click();
+      wd.findElement(By.name("group_footer")).clear();
+      wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  // инициализировать создание новой группы
+  public void initGroupCreation() {
+      wd.findElement(By.name("new")).click();
+  }
+
+  public void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+  }
+
+  public void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+  }
+}
